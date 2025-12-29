437827936ef500bb92dd61993dd149f5fde67f37
Arpit <arpit.mishra@microprixs.in>
2025-12-29 21:40:18 +0530

    Fastapi relationships

--- blogs/models.py
+++ blogs/models.py
@@ -1,11 +1,14 @@
 from database import Base
-from sqlalchemy import Column,Integer,String,Float
+from sqlalchemy import Column,Integer,String,Float,ForeignKey
+from sqlalchemy.orm import relationship
 
 class Blog(Base):
     __tablename__ = 'blogs'
     id = Column(Integer,primary_key=True,index=True)
     title=Column(String(50))
     body = Column(String(255))
+    user_id = Column(Integer,ForeignKey("users.id"))
+    creator = relationship("User",back_populates="blogs")
     
     
 class User(Base):
@@ -14,3 +17,4 @@ class User(Base):
     name=Column(String(70))
     email = Column(String(50))
     password = Column(String(255))
+    blogs = relationship("Blog",back_populates="creator")
