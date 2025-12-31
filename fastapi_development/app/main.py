dc07c20a09e523f0285eb8f7c3edc128bfb2308e
Arpit <arpit.mishra@microprixs.in>
2025-12-31 22:51:24 +0530

    Pydantic Model Schema creation with get product using id created

--- fastapi_development/app/main.py
+++ fastapi_development/app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI,HTTPException,Query
+from fastapi import FastAPI,HTTPException,Query,Path
 from services.product import get_all_products
+from schemas import Product
 
 app = FastAPI()
 
@@ -23,7 +24,7 @@ def root():
         
 @app.get("/products")
 
-def fetch_product(name:str = Query(default=None,min_length=2,max_length=50,description="enter product name"),sort_by_price:bool = Query(default=False,description="sort products by price"),order:str=Query(default="asc",description="sort products by ascending or descending order")):
+def fetch_product(name:str = Query(default=None,min_length=2,max_length=50,description="enter product name"),sort_by_price:bool = Query(default=False,description="sort products by price"),order:str=Query(default="asc",description="sort products by ascending or descending order"),limit:int = Query(default=5,ge=1,le=100,description="Number of Items to return"),offset:int = Query(default=0,ge=0,description="Pagination Offset")):
     if not name:
         return {
             "status":True,
@@ -35,21 +36,47 @@ def fetch_product(name:str = Query(default=None,min_length=2,max_length=50,descr
     
     
     product_name = name.strip().lower()
-    if sort_by_price:
-        reverse = order == "desc"
-        products = sorted(products,key=lambda product:product.get("price",0),reverse=reverse)
         
     if len(products) > 1:
         products = [product for product in products if product_name in product.get("name","").lower()]
         
-        return {
-            "status":True,
-            "message":"Products fetched successfully!",
-            "total_products":len(products),
-            "data" : products,
-        }
+        
         
     if not products:
         raise HTTPException(status_code=404,detail="No Products are found!")
     
+    if sort_by_price:
+        reverse = order == "desc"
+        products = sorted(products,key=lambda product:product.get("price",0),reverse=reverse)
+        
+        
+    return {
+            "status":True,
+            "message":"Products fetched successfully!",
+            "total_products":len(products),
+            "data" : products[offset:limit+offset],
+        }
     
+    
+    
+@app.get("/product/{id}")
+
+def get_product_detail(id:str = Path(...,min_length=36,max_length=36,description="UUID of the product")):
+    products = get_all_products()
+    
+    product = [product for product in products if product["id"] == id][0]
+    
+    if not product:
+        raise HTTPException(status_code=404,detail="Product Not Found!")
+    
+    return {
+        "status":True,
+        "message":"Product found!",
+        "data":product
+    }
+    
+    
+@app.post("/products",status_code=201)
+
+def create_product(product:Product):
+    pass
